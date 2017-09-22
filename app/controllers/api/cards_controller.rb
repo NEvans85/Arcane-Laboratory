bfd3bb22814f7e76b175cd41ac69a6bbde961cbd
Nicholas Evans <nevans85.pro@gmail.com>
2017-09-21 18:40:44 -0700

    add structure for backend to enable card search functionality

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,2 +1,20 @@
 class Api::CardsController < ApplicationController
+
+  def index(card_params)
+
+  end
+
+  def show(card_hash_id)
+
+  end
+
+  def card_params
+    params.require(card).permit(
+      :name,
+      :cmc,
+      :colors,
+      :exact_colors?,
+      :type,
+      :set)
+  end
 end
