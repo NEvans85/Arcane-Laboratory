8953c504ccfcbc4f184cbac884fcfe615e9eadf8
Nicholas Evans <nevans85.pro@gmail.com>
2017-09-22 10:56:31 -0700

    add auth and protected routes

--- frontend/util/root_util.jsx
+++ frontend/util/root_util.jsx
@@ -5,4 +5,26 @@ import { Redirect, Route, withRouter } from "react-router-dom";
 const mapStateToProps = state => ({
   loggedIn: Boolean(state.session.currentUser);
 });
-const Protected = { component: Component, path };
+
+const Auth = ({component: Component, path, loggedIn}) => (
+  <Route path={path} render={(props) => (
+      !loggedIn ? (
+        <Component {...props} />
+        ):(
+          <Redirect to="/"/>
+        )
+      )} />
+  );
+
+const Protected = ({component: Component, path, loggedIn}) => (
+   <Route path={path} render={(props) => (
+      loggedIn ? (
+        <Component {...props} />
+        ):(
+          <Redirect to="/login"/>
+        )
+      )} />
+  );
+
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected));
