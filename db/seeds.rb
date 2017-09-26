246af71346977536990ab2bf7a5b0fdcf16947c8
Nicholas Evans <nevans85.pro@gmail.com>
2017-09-26 07:34:18 -0700

    update seed and models to reflect removal of subtypes/supertypes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,6 @@
 
 User.create(username: "DemoUser", password: "DemoPassword", email: "Demo@email.com")
 
-subtypes = MTG::Subtype.all
-
-subtypes.each do |subtype|
-  Subtype.create(name: subtype)
-end
-
-supertypes = MTG::Supertype.all
-
-supertypes.each do |supertype|
-  Supertype.create(name: supertype)
-end
 
 5.times do |i|
   User.create(username: "User#{i + 1}",
@@ -38,65 +27,3 @@ silly_cards = MTG::Card.where(set: 'unh,ugl').all
     puts "#{c.name} added to #{d.title}"
   end
 end
-# Color.create(name: "White", symbol: "W")
-# Color.create(name: "Blue", symbol: "U")
-# Color.create(name: "Black", symbol: "B")
-# Color.create(name: "Red", symbol: "R")
-# Color.create(name: "Green", symbol: "G")
-
-# allSets = MTG::Set.all
-#
-# allSets.each do |expansion|
-#   Expansion.create(name: expansion.name, code: expansion.code)
-# end
-
-# allCards = MTG::Card.all
-# allCards = []
-# 343.times do |idx|
-#   allCards.concat(MTG::Card.where(page: idx).where(pageSize: 100).all)
-# end
-# stdSets = %w[kld aer akh hou]
-# stdSets = ["kld"]
-# stdCards = []
-# stdSets.each do |set|
-#   stdCards.concat(MTG::Card.where(set: set).all)
-# end
-
-# stdCards.each do |card|
-#   to_add = Card.new
-#   to_add.name = card.name
-#   to_add.card_hash_id = card.id
-#   to_add.mana_cost = card.mana_cost
-#   to_add.cmc = card.cmc
-#   to_add.card_type = card.type
-#   to_add.rarity = card.rarity
-#   to_add.rules_text = card.text
-#   to_add.flavor_text = card.flavor
-#   to_add.power = card.power
-#   to_add.toughness = card.toughness
-#   to_add.loyalty = card.loyalty
-#   to_add.image_url = card.image_url
-#   card_exp = Expansion.find_by(code: card.set)
-#   to_add.expansion_id = card_exp.id
-#   to_add.save
-#   unless card.colors.nil?
-#     card.colors.each do |color|
-#       puts "#{card.name} is #{color}"
-#       card_color = Color.find_by(name: color)
-#       CardColor.create!(card_id: card.id, color_id: card_color.id)
-#     end
-#   end
-#   # color_identity does not seem to be valid on the remote gem server yet.
-#   # unless card.color_identity.nil?
-#   #   card.color_identity.each do |color_symbol|
-#   #     puts "#{card.name} identifies as #{color_symbol}"
-#   #     color = Color.find_by(symbol: "#{color_symbol}")
-#   #     ColorIdentity.create!(card_id: card.id, color_id: color.id)
-#   #   end
-#   # end
-#   card.printings.each do |printing|
-#     expansion = Expansion.find_by(code: printing)
-#     CardPrinting.create(card_id: card.id, expansion_id: expansion.id)
-#   end
-#   puts "#{card.name} seeded."
-# end
