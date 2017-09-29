12fee54121e65790179e3df5158d4340eec6a373
Nicholas Evans <nevans85.pro@gmail.com>
2017-09-29 16:01:47 -0700

    fix title on deck view

--- frontend/components/MainContent/DeckView/DeckHeader/deck_header.jsx
+++ frontend/components/MainContent/DeckView/DeckHeader/deck_header.jsx
@@ -15,10 +15,13 @@ class DeckHeader extends React.Component {
     return (
       <div className="deck-header">
         <h2 className="deck-title">{this.title}</h2>
-        <h3 className="deck-details">
-          Created By: {this.creatorName} {this.createdAt} ago, Last Updated {" "}
-          {this.updatedAt} ago, Upvotes: {this.upvotes}
-        </h3>
+        <div className="deck-details">
+          <p>
+            Created By: {this.creatorName} {this.createdAt} ago
+          </p>
+          <p>Last Updated {this.updatedAt} ago</p>
+          <p> Upvotes: {this.upvotes} </p>
+        </div>
         <p className="deck-description">{this.description}</p>
       </div>
     );
